8de65ef9d0345ad0d494549f4ee3d400798560a7
mtorrisi <torrisi.mario@gmail.com>
2017-09-26 18:44:35 +0200

    Test fail travis.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var db;
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
-    console.log(err);
+    console.log(err)
     process.exit(1);
   }
 
